06c694097aeff0ebf4194212e40765fe5aec0828
Joyal J Joy <94773247+JoyalJJoy@users.noreply.github.com>
2025-07-06 12:32:23 +0530

    Create Buyer.java

---
+++ Buyer.java
@@ -0,0 +1,54 @@
+public class Buyer {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    
+    // Default constructor
+    public Buyer() {}
+    
+    // Constructor with all fields
+    public Buyer(int id, String name, String email, String phone, String address) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+    
+    // Constructor without ID (for new buyers)
+    public Buyer(String name, String email, String phone, String address) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+    
+    // Getters and Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+    
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+    
+    @Override
+    public String toString() {
+        return "Buyer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+} 
